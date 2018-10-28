e680e6d13b3e2cbfb86f62bf14344022f895670a
Sadik Ay <sadikay91@gmail.com>
2018-10-29 02:20:07 +0300

    webpacker asset pipeline both

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,28 +1,27 @@
-import TurbolinksAdapter from 'vue-turbolinks';
 import Vue from 'vue/dist/vue.esm'
-// import VueResource from 'vue-resource'
 
-// import "./../src/application.css"
+import TurbolinksAdapter from 'vue-turbolinks';
+import VueResource from 'vue-resource'
 
-// Vue.use(VueResource);
+Vue.use(VueResource);
 Vue.use(TurbolinksAdapter);
 
 // import components
-// import SampleVueComponent from '../components/sample.vue'
-// Vue.component('sample-vue-component', SampleVueComponent);
+import SampleVueComponent from '../components/sample.vue'
+Vue.component('sample-vue-component', SampleVueComponent);
 
 console.log("Loading...");
 
 document.addEventListener('turbolinks:load', () => {
-  console.log("Turbolinks Loaded");
-  // Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]')
-  //   .getAttribute('content');
+  console.log("Turbolinks Loaded...");
+  Vue.http.headers.common['X-CSRF-Token'] = document.querySelector('meta[name="csrf-token"]')
+    .getAttribute('content');
 
   // let token = document.getElementsByName('csrf-token')[0].getAttribute('content');
   // axios.defaults.headers.common['X-CSRF-Token'] = token;
   // axios.defaults.headers.common['Accept'] = 'application/json';
-  //
-  // const app = new Vue({
-  //   el: '[data-behavior="vue"]',
-  // });
+
+  const app = new Vue({
+    el: '[data-behavior="vue"]',
+  });
 });
